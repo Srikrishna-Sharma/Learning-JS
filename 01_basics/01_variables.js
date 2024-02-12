e6368fd697f9ab0c7f8da29e4551c22df450aa21
srikrishna-sharma <srikrishnasharma8@gmail.com>
2024-02-12 21:45:59 +0530

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+let accountEmail = 'srikrishnasharma8@gmail.com'
+var accoutPass = 'password' //never use var beacuse of the block scope and functional scope issue.
+accountCity = "Jaipur"
+let accountState;
+
+console.table([accountId,accountEmail,accoutPass,accountCity,accountState])
